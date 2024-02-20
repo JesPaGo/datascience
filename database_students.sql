1101117b833efd3dce01619a69e789cda838bffb
JesPaGo <134387373+JesPaGo@users.noreply.github.com>
2024-02-20 11:02:31 +0100

    Add files via upload

---
+++ database_students.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE student;
+use student;
+
+CREATE TABLE students(
+    StudentID int not null AUTO_INCREMENT,
+    FirstName varchar(100) NOT NULL,
+    Surname varchar(100) NOT NULL,
+    PRIMARY KEY (StudentID)
+);
+
+INSERT INTO students(FirstName, Surname)
+VALUES ('John', 'Doe'),
+       ('Jane', 'Doe'),
+       ('Bob', 'Smith'),
+       ('Alice', 'Smith');
